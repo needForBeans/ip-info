49017323627302a4f6956e921f8fbf0bf2ccba82
needForBeans <109034482+needForBeans@users.noreply.github.com>
2022-10-04 18:04:11 +0200

    1.2.0

--- src/utils/formatCsv.js
+++ src/utils/formatCsv.js
@@ -10,17 +10,13 @@ module.exports = (inFile, dataFolder, formatFunction, config) => {
     try {
       if (
         typeof inFile !== 'string' ||
-        //typeof dataFolder !== 'string' ||
         typeof formatFunction !== 'function'
       ) throw 'invalid function input'
       
       if (!fs.existsSync(inFile)) throw `could not find file: ${inFile}` 
-      //if (!fs.existsSync(dataFolder)) throw `could not find data folder: ${dataFolder}`
       config = typeof config === 'object' ? config : {}
 
-      
-      const formatted = { linked: {} } // save data so you dont need to import it after
-      const writeStreams = { linked: {} } // save to files
+      const formatted = { linked: {} } 
 
       const rl = readline.createInterface({
         input: fs.createReadStream(inFile)
@@ -49,29 +45,8 @@ module.exports = (inFile, dataFolder, formatFunction, config) => {
           // only save item when version is set 
           // (used for datasets with only linked data, keep in mind to set the item link either way to prevent errors)
           if (typeof result.version === 'number') { 
-
-            /* if (typeof writeStreams[`v${result.version}`] !== 'object') {
-              const wsFile = path.join(dataFolder, `/v${result.version}.json`)
-              if (fs.existsSync(wsFile)) throw { message: `cant write to existing database: ${wsFile}`, critical: true } 
-
-              writeStreams[`v${result.version}`] = {
-                fd: fs.openSync(wsFile, 'w'),
-                useComma: false
-              }
-              
-              fs.writeSync(writeStreams[`v${result.version}`].fd, '[')
-            }
-            
-            if (writeStreams[`v${result.version}`].useComma === true) {
-              fs.writeSync(writeStreams[`v${result.version}`].fd, `,\n  ${JSON.stringify(result.item)}`)
-            } else {
-              writeStreams[`v${result.version}`].useComma = true 
-              fs.writeSync(writeStreams[`v${result.version}`].fd, `\n  ${JSON.stringify(result.item)}`)
-            } */
-
             if (!Array.isArray(formatted[`v${result.version}`])) formatted[`v${result.version}`] = []
             formatted[`v${result.version}`].push(result.item)
-
           }
 
           // creates linked list to save space
@@ -82,31 +57,7 @@ module.exports = (inFile, dataFolder, formatFunction, config) => {
             if (typeof formatted.linked[key] !== 'object') formatted.linked[key] = {}
             
             if (typeof formatted.linked[key][result.item[key]] === 'undefined') {
-
-              /* const linkedPath = path.join(dataFolder, '/linked')
-              
-              if (!fs.existsSync(linkedPath)) fs.mkdirSync(linkedPath)
-              
-              if (typeof writeStreams.linked[key] !== 'object') {
-                const wsFile = path.join(linkedPath, `/${key}.json`)
-
-                writeStreams.linked[key] = {
-                  fd: fs.openSync(wsFile, 'w'),
-                  useComma: false 
-                }
-                
-                fs.writeSync(writeStreams.linked[key].fd, '{')
-              } 
-
-              if (writeStreams.linked[key].useComma === true) {
-                fs.writeSync(writeStreams.linked[key].fd, `,\n  "${result.item[key]}": ${JSON.stringify(entry)}`)
-              } else {
-                fs.writeSync(writeStreams.linked[key].fd, `\n  "${result.item[key]}": ${JSON.stringify(entry)}`)
-                writeStreams.linked[key].useComma = true
-              } */
-              
               formatted.linked[key][result.item[key]] = entry
-
             } else if (
               typeof formatted.linked[key][result.item[key]] !== 'object' ||
               typeof entry !== 'object' ||
@@ -124,70 +75,6 @@ module.exports = (inFile, dataFolder, formatFunction, config) => {
 
       if (Object.keys(formatted.linked).length <= 0) delete formatted.linked  
       if (Object.keys(writeStreams.linked).length <= 0) delete writeStreams.linked  
-
-      /* save data to files */
-      /* for (const [key, entry] of Object.entries(formatted)) {
-        if (key === 'linked') {
-          if (typeof entry !== 'object') throw new Error('expected object data')
-
-          for (const [lKey, lEntry] of Object.entries(entry)) {
-            if (typeof lEntry !== 'object') throw new Error('expected object data')
-
-            const formatedPath = path.join(dataFolder, '/formatted')
-            const filePath = path.join(formatedPath, `/${lKey}.json`)
-
-            if (!fs.existsSync()) fs.mkdirSync(formatedPath)
-
-            const fd = fs.openSync(filePath)
-            fs.writeSync(fd, '{')
-            let useComma = false
-
-            for (const [k, i] of Object.entries(lEntry)) {
-              if (useComma === true) fs.writeSync(fd, `,\n  "${k}": ${i}`)
-              else {
-                fs.writeSync(fd, `\n  "${k}": ${i}`)
-                useComma = true
-              }
-            }
-
-            fs.writeSync(fd, '\n}')
-            fs.closeSync(fd)
-          }
-
-        } else {
-          if (!Array.isArray(entry)) throw new Error('expected array data')
-
-          const fd = fs.openSync(path.join(dataFolder, `/${key}.json`), 'w')
-          fs.writeSync(fd, '[')
-          let useComma = false
-
-          for (const item of entry) {
-            if (useComma === true) fs.writeSync(fd, ',\n  ' + JSON.stringify(item))
-            else {
-              fs.writeSync(fd, '\n  ' + JSON.stringify(item))
-              useComma = true
-            }
-          }
-
-          fs.writeSync(fd, '\n]')
-          fs.closeSync(fd)
-        }
-      }  */
-
-      /* end write streams */
-      /* Object.entries(writeStreams).map(([key, entry]) => {
-        if (key === 'linked') Object.entries(entry).map(([lKey, lEntry]) => {
-          if (typeof lEntry === 'object' && typeof lEntry.fd !== 'undefined') {
-            fs.writeSync(lEntry.fd, '\n}')
-            fs.closeSync(lEntry.fd)
-          }
-        })
-        else if (typeof entry === 'object' && typeof entry.fd !== 'undefined') {
-          fs.writeSync(entry.fd, '\n]')
-          fs.closeSync(entry.fd)
-        }
-        else log.error(`[formatCsv] failed to end writeStream ${key}`)
-      }) */
 
       /* get meta info */
       Object.entries(formatted).map(([key, entry]) => {
